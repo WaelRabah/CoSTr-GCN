06940ffe1801695a921781e7ca7f2bd727995027
WaelRabah <wael.rabah@insat.u-carthage.tn>
2022-07-29 10:43:11 +0200

    initial

--- main.py
+++
@@ -1,8 +0,0 @@
-from train import train_model
-
-
-
-if __name__ == '__main__':
-    # The dataset can be changed into either 'BRIAREO' or 'FPHA'
-    dataset_name="SHREC17"
-    train_model(dataset_name)
